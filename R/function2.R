115cac118e2d334862449551fc6776cf37387212
janclod <ctiecher@gmail.com>
2020-12-26 17:20:23 +0100

    Restored function2.R

--- R/function2.R
+++ R/function2.R
@@ -9,5 +9,5 @@
 function2 <- function(y) {
   c1 <- as.numeric(y$col1)
   c2 <- as.numeric(y$col2)
-  data.frame(col1 = c1, col2 = c2) %>% stats::na.omit()
+  stats::na.omit(data.frame(col1 = c1, col2 = c2))
 }
